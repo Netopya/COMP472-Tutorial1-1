a28a9d9f0b154fd815f4e92ab4df9f8000a3b3b5
Netopya <netopya@netopyaplanet.com>
2016-09-22 12:41:28 -0400

    Initial in class work commited

---
+++ COMP472_A_Search_Question/COMP472_A_Search_Question/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP472_A_Search_Question
+{
+    public class Node
+    {
+        public int heuristic;
+        public List<Tuple<int, Node>> operations = new List<Tuple<int, Node>>();
+        public List<Node> path = new List<Node>();
+        public Node parent;
+
+        public Node(int heuristic)
+        {
+            this.heuristic = heuristic;
+        }
+
+        public void MakeLink(int cost, Node node)
+        {
+            operations.Add(new Tuple<int, Node>(cost, node));
+        }
+
+        public int getCost(Node from)
+        {
+            if(from == this)
+            {
+                return 0;
+            }
+
+            if(parent == null && from == null)
+            {
+                throw new Exception();
+            }
+
+            int childCost = operations.First(x => x.Item2 == from).Item1;
+
+            if (parent == null)
+            {
+                return childCost;
+            }
+
+            return parent.getCost(this) + childCost;
+        }
+    }
+}
---
+++ COMP472_A_Search_Question/COMP472_A_Search_Question/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+
+namespace COMP472_A_Search_Question
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Node S = new Node(10);
+            Node A = new Node(5);
+            Node B = new Node(8);
+            Node C = new Node(3);
+            Node D = new Node(2);
+            Node E = new Node(4);
+            Node G1 = new Node(0);
+            Node G2 = new Node(0);
+
+            S.MakeLink(3, A);
+            S.MakeLink(7, B);
+            A.MakeLink(1, C);
+            A.MakeLink(6, D);
+            B.MakeLink(1, E);
+            B.MakeLink(9, G2);
+            C.MakeLink(2, S);
+            C.MakeLink(4, D);
+            D.MakeLink(6, G1);
+            D.MakeLink(3, B);
+            E.MakeLink(5, G2);
+            G1.MakeLink(2, C);
+            G2.MakeLink(8, B);
+
+            List<Node> frontier = new List<Node>();
+            List<Node> visited = new List<Node>();
+
+            frontier.Add(S);
+
+            Node result;
+            while (true)
+            {
+                Node current = frontier.MinBy(x => S.getCost(x) + x.heuristic);
+
+                if(current == G1 || current == G2)
+                {
+                    result = current;
+                    break;
+                }
+
+                frontier.Remove(current);
+                visited.Add(current);
+                foreach(Tuple<int, Node> ops in current.operations)
+                {
+                    if (visited.Contains(ops.Item2))
+                        continue;
+
+                    if(frontier.Contains(ops.Item2))
+                    {
+                        Node oldParent = ops.Item2.parent;
+                        int oldCost = S.getCost(ops.Item2);
+
+                        ops.Item2.parent = current;
+                        int newCost = S.getCost(ops.Item2);
+
+                        if(oldCost < newCost)
+                        {
+                            ops.Item2.parent = oldParent;
+                        }
+                        continue;
+                    }
+
+                    ops.Item2.parent = current;
+                    frontier.Add(ops.Item2);
+                }
+            }
+
+            Console.WriteLine("Found something!");
+        }
+    }
+}
